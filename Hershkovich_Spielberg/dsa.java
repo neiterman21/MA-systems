aca1de58d1bc7f2dd531279386cc0a55f3a613ae
Yitzhak <yspielb@gmail.com>
2019-12-25 19:59:36 +0200

    syntax corrections

--- Hershkovich_Spielberg/dsa.java
+++ Hershkovich_Spielberg/dsa.java
@@ -1,85 +1,95 @@
+public class Globals {
+   public static float p=0.1;
+   public static char[] possible_colors={'r','g','b'};
+   ///
+}
 
-global float p
-global char[] possible_colors={'r','g','b'}
 
-class color_gain_tuple{
-	public char color 
-	public int gain
+public class color_gain_tuple{
+	public char color; 
+	public int gain;
 	public color_gain_tuple(char color,int gain){
-		this.color=color
-		this.gain=gain
+		this.color=color;
+		this.gain=gain;
 	}
 }
 
-class Agent{
-	char my_color
-	Agent[] neighbours
-
+public class Agent{
+	public char color;
+	public Agent[] neighbours;
+	
 	public Agent(){
 		// set this.neighbours --Anylogic
-		this.current_value=random.sample(possible_values)
+		this.color=random.sample(Globals.possible_colors);
 	}
 
-	public get_zerogain_colors(){
-		char[] zerogain_colors={}
-		int current_cost=compute_cost(this.my_color)
-		for color in possible_colors
-			if color!=this.my_color and (current_cost-compute_cost(color)==0)
-				zerogain_colors.append(color)
+	public List<char> get_zerogain_colors(){
+		List<char> zerogain_colors=new ArrayList<char>();
+		int current_cost=this.compute_cost(this.color);
+		for (char color:Globals.possible_colors){
+			if color!=this.color and (current_cost-this.compute_cost(color)==0){
+				zerogain_colors.add(color);
+			}
+		}
 		return zerogain_colors
 	}
 
 	public int compute_cost(char color){
 		int cost=0
-		for nb in this.neigbours
-			if nb.color==color
-				cost+=1
+		for (Agent nb:this.neigbours){
+			if (nb.color==color) {
+				cost+=1;
+			}
+		}
 		return cost
 	}
 
 
+	public color_gain_tuple compute_gain(){
+		int current_cost=this.compute_cost(this.color);
+		int best_gain=0;
+		int cost;
+		int gain;
+		char best_color;
+		for (char color:Globals.possible_colors){
+			cost=this.compute_cost(color);
+			gain=current_cost-cost;
+			if (gain>best_gain){
+				best_gain=gain;
+				best_color=color;
+			}
+		}
 
-	public void calc_gain(){
-		int current_cost=compute_cost(this.color)
-		int best_gain=0
-		int cost
-		int gain
-		char best_color
-		for color in possible_colors
-			cost=compute_cost(color)
-			gain=current_cost-cost
-			if gain>best_gain
-				best_gain=gain
-				best_color=color
-
-		return color_gain_tuple(best_color,best_gain)
+		return new color_gain_tuple(best_color,best_gain)
 	}
 
-	/*
+	
+	public void dsa_step(){
+		best_color_and_gain=this.compute_gain();
+		my_gain=best_color_and_gain.gain;
+		if (best_color_and_gain.gain>0){
+			this.color=best_color_and_gain.color;
+		}
+		else{
+			zerogain_colors=this.get_zerogain_colors()
+			if  ((zerogain_colors not empty) and (random()<Globals.p)){
+				this.color=random.sample(zerogain_colors);
+			}
+		}
+
+	}
+
+
+/*
 	public void calc_neighbour_gains(){
 		int[] neighbour_gains={}
 		color_gain_tuple color_and_gain
 		for ag in neigbours
-			color_and_gain=ag.calc_gain()
+			color_and_gain=ag.compute_gain()
 			neighbour_gains.append(color_and_gain.gain)
 		return neighbour_gains
 	}
     */
-	public void dsa_step(){
-		best_color_and_gain=calc_gain()
-		my_gain=best_color_and_gain.gain
-		if best_color_and_gain.gain>0
-			this.my_color=best_color_and_gain.color
-		else
-			zerogain_colors=get_zerogain_colors()
-			if  (zerogain_colors not empty) and (random()<p)
-				this.my_color=random.sample(zerogain_colors)
-
-
-	}
-
-
-
 
 
 
